b8ddfa5fec86d819b6ebcb9e798af8cacaddc2a2
Nayan Anand <125741892+nayananandofficial@users.noreply.github.com>
2025-10-04 22:42:53 +0530

    update

--- backend/routes/experimentRoute.js
+++ backend/routes/experimentRoute.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { createUserExperiment } from '../controllers/experimentController.js';
 
-
 const router = express.Router();
 router.post('/', createUserExperiment) ; 
 
